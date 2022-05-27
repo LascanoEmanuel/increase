f207a7816882f9f1a1dcb9ae65fce112bf22308b
Emanuel <lascano.emanuel@gmail.com>
2022-05-27 12:35:33 -0300

    Segunda Entrega

--- tiendaincrease/src/App.js
+++ tiendaincrease/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/itemListContainer/itemListContainer';
+
 
 function App() {
   return (
-    <div className="App">
+    
+    <div className="App"> 
+    <nav className='Navbar'>
+    <NavBar />
+     </nav>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <ItemListContainer greeting="Bienvenidos"/>
       </header>
     </div>
   );
---
+++ tiendaincrease/src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+
+const NavBar =() => {
+    return (
+       <nav>
+ <h1> Increase</h1>
+ <button>Kit Cultivo</button>
+ <button>Carpas</button>
+ <button>Lamparas</button>
+ <button>Accesorios</button>
+
+       </nav> 
+    )
+}
+export default NavBar
